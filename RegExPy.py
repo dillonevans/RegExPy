c82bceade55c849f1eaa97b5abcd7f5a332ca3da
dillonevans <dillon.e.evans@okstate.edu>
2021-11-15 19:57:06 -0600

    Added RegExPy.py

---
+++ RegExPy.py
@@ -0,0 +1,12 @@
+EPS = "epsilon"
+
+class DFA: 
+    def __init__(self) -> None:
+        self.isAcceptState = False 
+        self.list = [] 
+        
+p1 = DFA()
+p2 = DFA()
+p2.isAcceptState = True
+
+
